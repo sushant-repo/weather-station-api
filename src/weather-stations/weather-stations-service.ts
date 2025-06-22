44e16607d4f73037533397e567ffd754e734642a
Sushant <sus.hant@outlook.com>
2025-06-23 09:45:27 +1000

    parses weather_stations.csv and seeds WeatherStations

--- src/weather-stations/weather-stations-service.ts
+++ src/weather-stations/weather-stations-service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { WeatherStation } from "./weather-station-entity";
 import { Repository } from "typeorm";
+import { WeatherStation } from "./weather-station-entity";
+import * as path from "path";
+import * as fs from "fs";
+import * as csv from "csv-parser";
+
+interface WeatherStationRaw {
+    id: string,
+    ws_name: string,
+    site: string,
+    portfolio: string,
+    state: string,
+    latitude: string,
+    longitude: string,
+}
 
 @Injectable()
 export class WeatherStationService {
-    
     constructor(
         @InjectRepository(WeatherStation)
-        private readonly repo: Repository<WeatherStation>
+        private repo: Repository<WeatherStation>
     ){}
 
     getAllStations() {
@@ -16,25 +28,45 @@ export class WeatherStationService {
     }
 
     async onModuleInit(): Promise<void>{
+        const filePath = path.resolve(
+            process.cwd(),
+            "src",
+            "seeds",
+            "weather_stations.csv"
+        );
+
+        const stations: WeatherStation[] = [];
+
+        const fileParser = ():Promise<WeatherStation[]> => {
+            return new Promise((resolve, reject) => {
+                fs.createReadStream(filePath)
+                .pipe(csv())
+                .on("data", (row: WeatherStationRaw) => {
+                      const station =  this.repo.create({
+                            id: parseInt(row.id, 10),
+                            latitude: parseFloat(row.latitude),
+                            longitude: parseFloat(row.longitude),
+                            portfolio: row.portfolio,
+                            site: row.site,
+                            state: row.state,
+                            ws_name: row.ws_name
+                        })
+                    ;
+                    stations.push(station)
+                })
+                .on("end", () => resolve(stations))
+                .on("error", reject);
+            });
+        }
+
         const count = await this.repo.count();
 
         if(count === 0){
-            const stations = this.repo.create([
-                {
-                    "id": 1,
-                    "ws_name": "Cohuna North",
-                    "site": "Cohuna Solar Farm",
-                    "portfolio": "Enel Green Power",
-                    "state": "VIC",
-                    "latitude": 145.449895817713,
-                    "longitude": 144.217208,
-                },
-            ])
-
-            this.repo.save(stations);
-            console.log(`Seeded ${stations.length} weather stations`);
+            const parsedStations = await fileParser();
+            await this.repo.save(parsedStations);
+            console.log(`Seeded ${parsedStations.length} Weather Stations.`)
         }else{
-            console.log("Stations already exists. Skipping seeding");
+            console.info("Weather stations already exists. Skipping seeding");
         }
     }
 }
