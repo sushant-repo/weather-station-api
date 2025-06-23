cb9024b9d53a89a7a3c6bdca2154d8d3ea7e67d0
Sushant <sus.hant@outlook.com>
2025-06-23 11:55:09 +1000

    adds swagger ui for documentation on APIs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+   const config = new DocumentBuilder()
+    .setTitle('Weather Stations')
+    .setDescription('The weather stations APIs')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
